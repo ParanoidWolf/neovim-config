11d157ae7862c77dcf2e6beb19718553110f4a63
P H Aju <ajuph9224@gmail.com>
2022-02-22 21:21:32 +0000

    remapped leader to space

--- lua/config.lua
+++ lua/config.lua
@@ -4,3 +4,5 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.autoindent = true
 vim.cmd [[colorscheme gruvbox]]
+vim.api.nvim_set_keymap('n', ' ', '<NOP>', { noremap = true, silent = true })
+vim.g.mapleader = ' '
--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,4 +1,5 @@
 -- Keymaps
 local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true }
+local opts = { noremap = true, silent = true }
 keymap('n', 'Y', 'y$', opts)
+keymap('n', '<Leader>v', ':e ~/.config/nvim/init.lua<CR>', opts)
