a7e2d34f604a6c9f4fad62345803dda2ce36c4e2
P H Aju <ajuph9224@gmail.com>
2022-02-24 09:36:51 +0000

    lsp addons

--- lua/packages.lua
+++ lua/packages.lua
@@ -10,14 +10,18 @@ require('packer').startup(function()
         use 'vimwiki/vimwiki' -- Note taking
         use 'morhetz/gruvbox' -- Color scheme
         use 'neovim/nvim-lspconfig' -- Built-in LSP
+        use 'glepnir/lspsaga.nvim' -- Performant UI for LSP
+        use 'onsails/lspkind-nvim' -- Icons in LSP drop down
+        use 'kosayoda/nvim-lightbulb' -- VSCode like notification for code-action
         use 'hrsh7th/nvim-cmp' -- Auto completion
         use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
         use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
         use 'L3MON4D3/LuaSnip' -- Snippets plugin
         use { 'nvim-treesitter/nvim-treesitter', run = ":TSUpdate" } -- Treesitter syntax highlighting
         use 'kyazdani42/nvim-web-devicons' -- optional, for file icon
-        use {
-            'kyazdani42/nvim-tree.lua',
-        }
+        use 'kyazdani42/nvim-tree.lua' -- Nvim Tree file explorer
+        use 'glepnir/galaxyline.nvim' -- Status line
+        use 'nvim-lua/plenary.nvim' -- function library
+        use 'nvim-telescope/telescope.nvim' -- Advanced search tool
+        use 'nvim-telescope/telescope-media-files.nvim' -- Media preview
 end)
-
