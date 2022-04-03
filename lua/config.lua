a94af2da5076772a0e979c30f80f5e956b8905b5
Paranoid <ajuph9224@gmail.com>
2022-04-03 19:03:15 +0530

    added relative numbering to the setup

--- lua/config.lua
+++ lua/config.lua
@@ -23,6 +23,7 @@ vim.opt.ruler = true
 vim.opt.laststatus = 2
 vim.opt.showtabline = 2
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.background = "dark"
 vim.opt.showmode = false
